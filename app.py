0260a02412b3d52ed727443c786d32d97ea125e4
Torje Eikenes <torje.n.eikenes@gmail.com>
2019-02-03 21:43:22 +0100

    la til status

--- app.py
+++ app.py
@@ -1,24 +1,84 @@
-
-
 from flask import Flask, render_template, request, make_response, redirect
 from forms import LokForm
 import sys
 import csv
+import datetime
+import operator
+
+def pastWeeks(d, uke):
+    weeks = {}
+    for i in range(4, uke+1):
+        ukas = ukasLok(d,i)
+        #lok = max(ukas.items(), key=operator.itemgetter(1))[0]
+        lok = max(ukas, key=lambda k: ukas[k])
+        weeks[i] = lok
+
+    return weeks
+
+def ukasLok(d, week):
+    ukas = {}
+    for i in d:
+        uke = int(datetime.datetime.strptime(i['dato'], '%Y-%m-%d').strftime("%V"))
+        if uke == week:
+            if i['navn'] in ukas:
+                ukas[i['navn']] += int(i['antall'])
+            else:
+                ukas[i['navn']] = int(i['antall'])
+    return ukas
+
+def ukasStreker(d, week):
+    streker = []
+    for i in d:
+        uke = int(datetime.datetime.strptime(i['dato'], '%Y-%m-%d').strftime("%V"))
+        if uke == week:
+            streker.append(i)
+    return streker
+
+
+def readFile(file):
+    with open(file, 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        data = list(reader)
+        return data
+
+def findTotal(d):
+    total = {}
+    for i in d:
+        if i['navn'] in total:
+            total[i['navn']] += int(i['antall'])
+        else:
+            total[i['navn']] = int(i['antall'])
+    return total
 
 
 def writeToFile(navn,antall,forkl,dato):
     with open('svar.csv', mode='a') as lok_file:
-        lok_writer = csv.writer(lok_file, delimiter=',')
-        row = [navn, antall, forkl, dato]
+        fieldnames = ['navn', 'antall', 'forklaring', 'dato']
+        lok_writer = csv.DictWriter(lok_file, fieldnames=fieldnames)
+        row = {'navn': navn, 'antall': antall,'forklaring': forkl,'dato': dato}
+        #row = {'navn': 'torge', 'antall': 2,'forklaring': 'sgsdsdfsdf','dato': '2019-03-04'}
         lok_writer.writerow(row)
     lok_file.close()
-
-
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hemmelig'
 
+@app.route('/')
+def index():
+    data = readFile('svar.csv')
+    total = findTotal(data)
+    thisWeek = int(datetime.datetime.now().strftime("%V"))
+    ukas = ukasLok(data, thisWeek)
+    past = pastWeeks(data, thisWeek)
+    return render_template('index.html', data=data, total=total, ukas=ukas, past=past)
+
+@app.route('/uke/<ukeNr>')
+def uke(ukeNr):
+    data = readFile('svar.csv')
+    ukas = ukasLok(data, int(ukeNr))
+    streker = ukasStreker(data, int(ukeNr))
+    return render_template('uke.html',ukas=ukas, ukeNr=ukeNr, streker=streker)
 
 @app.route('/submit', methods=['GET', 'POST'])
 def submit():
@@ -31,10 +91,6 @@ def submit():
         dato = form.dato.data
         writeToFile(navn,lokstreker,forklaring,dato)
 
-        print(navn, file=sys.stdout)
-        print(lokstreker, file=sys.stdout)
-        print(dato, file=sys.stdout)
-        print(forklaring, file=sys.stdout)
     return render_template('submit.html', form=form)
 
 if __name__ == "__main__":
